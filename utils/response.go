c652d38b5c9a8e6e57ec181b384468b6e732ecc9
tqjl21 <tqjl21@mails.tsinghua.edu.cn>
2023-03-21 12:12:01 +0800

    [feature:utils/response]update a function of error response(#-1)

--- utils/response.go
+++ utils/response.go
@@ -29,3 +29,16 @@ func (r *responseJson) Success(info string, data interface{}) {
 		Data: data,
 	})
 }
+
+/*
+return an error response
+method: http status code, example: http.StatusBadRequest
+*/
+func (r *responseJson) Error(method int, code int, info string, data interface{}) {
+	r.context.Abort()
+	r.context.JSON(method, ResponseData{
+		Code: code,
+		Info: info,
+		Data: data,
+	})
+}
