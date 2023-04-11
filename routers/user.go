d3cf1355b57e20b7897d5f8ab71117ba9c82da1a
陈学林 <2021010843@secoder.net>
2023-04-11 18:44:33 +0800

    [feature:dao/asset(#14)]Add router for the new user api

--- routers/user.go
+++ routers/user.go
@@ -28,6 +28,7 @@ func (user *userRouter) Init(group *gin.RouterGroup) {
 func (user *userRouter) routerNotNeedLogin(group *gin.RouterGroup) {
 	group.POST("/register", utils.Handler(api.UserApi.UserRegister))
 	group.POST("/login", utils.Handler(api.UserApi.UserLogin))
+
 }
 func (user *userRouter) routerNeedLogin(group *gin.RouterGroup) {
 	group.POST("/logout", utils.Handler(middleware.JWTMiddleware()), utils.Handler(api.UserApi.UserLogout))
@@ -35,4 +36,6 @@ func (user *userRouter) routerNeedLogin(group *gin.RouterGroup) {
 	group.PATCH("/:username", utils.Handler(middleware.JWTMiddleware()), utils.Handler((api.UserApi.ResetContent)))
 	group.GET("/:username/lock", utils.Handler(middleware.JWTMiddleware()), utils.Handler(api.UserApi.LockUser))
 	group.GET("/:username/unlock", utils.Handler(middleware.JWTMiddleware()), utils.Handler(api.UserApi.UnlockUser))
+	group.GET("/info/:user_id", utils.Handler(middleware.JWTMiddleware()), utils.Handler(api.UserApi.GetUserInfoByID))
+	group.GET("/list", utils.Handler(middleware.JWTMiddleware()), utils.Handler(api.UserApi.GetAllUsers))
 }
